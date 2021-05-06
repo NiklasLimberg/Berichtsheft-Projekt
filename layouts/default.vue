851a2c099eb8c1f9464e28f8cca8b079b155bc4c
Niklas Limberg <n.limberg@shopware.com>
2021-05-06 18:10:25 +0200

    Start project

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="min-h-screen flex justify-center items-center text-center mx-auto">
+    <div>
+      <Logo />
+      <div class="tracking-wider">
+        <h1 class="text-6xl">
+          Berichtsheft-Projekt
+        </h1>
+        <h2 class="text-4xl">
+          built with Vue {{ version }}
+        </h2>
+      </div>
+      <div class="p-6">
+        <a
+          href="https://nuxtjs.org/"
+          rel="noopener noreferrer"
+          class="m-1 p-4 px-3 border-green-400 hover:text-white hover:bg-green-400 border-2 rounded-md"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/NiklasLimberg/Berichtsheft-Projekt"
+          rel="noopener noreferrer"
+          class="m-1 p-4 px-3 border-grey-600 hover:text-white hover:bg-gray-600 border-2 rounded-md"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  computed: {
+    version: () => Vue.version
+  }
+})
+</script>
+
+<style>
+</style>
