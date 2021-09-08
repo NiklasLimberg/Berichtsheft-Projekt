6b21c4674b36b489dd20d55fd2377f469297c631
Niklas Limberg <n.limberg@shopware.com>
2021-09-08 08:59:46 +0200

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="min-h-screen flex justify-center items-center text-center mx-auto">
+    <header>
+      <h1 v-if="error.statusCode === 404">
+        Page not found
+      </h1>
+      <h1 v-else>
+        An error occurred
+      </h1>
+      <header>
+        <main class="mt-6">
+          <NuxtLink class="button-primary p-4 px-3" to="/">
+            Home page
+          </NuxtLink>
+        </main>
+      </header>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { NuxtError } from '@nuxt/types'
+
+export default Vue.extend({
+  props: {
+    error: {
+      type: Object as () => NuxtError,
+      required: true
+    }
+  }
+})
+</script>
